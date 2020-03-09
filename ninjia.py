ac6a45790cd1c4425092d15848407dd2c9afa7a9
your_name <your_email>
2020-03-09 11:58:39 +0800

    feat:  初步完成副本成绩识别、登记、标记功能
    1.  百度OCR提供图片识别功能
    2. openpyxl提供对excel的识别能力
    3. 添加事例
    4. 添加.exe可执行文件

--- ninjia.py
+++ ninjia.py
@@ -1,7 +1,10 @@
-# 引入load_workbook
 from openpyxl import load_workbook
+from openpyxl.styles import Color, PatternFill, Border,Side
 from aip import AipOcr
 from fuzzywuzzy import fuzz
+import json
+import os
+import sys
 
 config = {
     'appId': '18723556',
@@ -9,11 +12,94 @@ config = {
     'secretKey': 'vtk5zbBVi4hoVWclNwedp1orfxjtfkOM'
 }
 
+FBType = {
+    '1': '蝙蝠',
+    '2': '西瓜',
+    '3': '金币',
+    '4': '飞镖',
+    '5': '礼带',
+    '6': '河豚',
+    '7': '宝箱',
+    '8': '无尽'
+}
+
+nameList = []
+
+excelPath = '胧月成绩统计表.xlsx'
+
+classifyJson = {
+    "color": {
+        "A": "4EAC5B",
+        "B": "A0CD62",
+        "C": "FFFD55",
+        "D": "F7C143",
+        "absence": "EA3323",
+        "leave": "BFBFBF"
+    },
+    "蝙蝠": {
+        "A": 390,
+        "B": 385,
+        "C": 380,
+        "max": 401
+    },
+    "西瓜": {
+        "A": 317,
+        "B": 315,
+        "C": 313,
+        "max":317
+    },
+    "金币": {
+        "A": 3090,
+        "B": 3080,
+        "C": 3070,
+        "max":3110
+    },
+    "飞镖": {
+        "A": 600,
+        "B": 598,
+        "C": 596,
+        "max": 601,
+    },
+    "礼带": {
+        "A": 76,
+        "B": 73,
+        "C": 70,
+        "max": 82
+    },
+    "河豚": {
+        "A": 185,
+        "B": 180,
+        "C": 175,
+        "max": 197
+    },
+    "宝箱": {
+        "A": 60,
+        "B": 58,
+        "C": 57,
+        "max": 62
+    },
+    "无尽": {
+        "A": 4067,
+        "B": 4066,
+        "C": 4065,
+        "max": 4067
+    }
+}
+
 client = AipOcr(**config)
 
+type = input('请选择副本类型\n1.蝙蝠 2.西瓜 3.金币 4.飞镖 5.礼带 6.河豚 7.宝箱 8.无尽\n:')
+
 def get_file_content(file):
-    with open(file, 'rb') as fp:
-        return fp.read()
+    print('识别中...', file)
+    try:
+        with open(file, 'rb') as fp:
+            return fp.read()
+    except:
+        print('打开文件失败')
+        exit(1)
+
+    
 
 def img_to_str(image_path):
     image = get_file_content(image_path)
@@ -24,57 +110,145 @@ def img_to_str(image_path):
             data.append(w['words'])
     return data
 
+
 def getImgInfo():
-    imagepath = 'pic.jpg'
-    rawResult = img_to_str(imagepath)
-    resultList = []
+    print('图片格式必须为: pic+数字，请务必修改')
+    photoType = input("请输入图片后缀(默认为: jpg):")
+    if photoType == '': photoType = 'jpg'
+    num = input("请输入图片数量:")
+    count = int(num)
     result = {}
-    for word in rawResult :
-        if word.find('新人') < 0 \
-            and word.find('族员') < 0 \
-            and word.find('精英') < 0 \
-            and word.find('豪杰') < 0 \
-            and word.find('长老') < 0 \
-            and word.find('副族长') < 0 \
-            and word.find('族长') <0:
-            resultList.append(word)
-    for i in range(0, len(resultList) - 1, 2) :
-        result[resultList[i]] = resultList[i+1]
+    for i in range(1,count+1):
+        imagepath = os.path.dirname(os.path.realpath(sys.executable)) + '/' + 'pic' + str(i) + '.' + photoType
+        rawResult = img_to_str(imagepath)
+        resultList = []
+        for word in rawResult :
+            if word.find('新人') < 0 \
+                and word.find('族员') < 0 \
+                and word.find('精英') < 0 \
+                and word.find('豪杰') < 0 \
+                and word.find('长老') < 0 \
+                and word.find('副族长') < 0 \
+                and word.find('族长') <0:
+                resultList.append(word)
+        j = 0
+        while j < len(resultList):
+            if j+1 == len(resultList) or not resultList[j+1].isdigit() or resultList[j+1] == '125800':
+                maxLength = 0
+                if type == '1' or type == '2' or type == '4' or type == '6': #最多3位数字
+                    maxLength = 3
+                elif type == '3' or type == '8': #最多4位数字
+                    maxLength = 4
+                else: #其余最多2位数字
+                    maxLength = 2
+                score = ''
+                # print('--------------开始分割成绩--------------')
+                # print(resultList[j])
+                # print('maxLength:', maxLength)
+                for k in range(-1, -maxLength - 1, -1):
+                    # print(resultList[j][-1])
+                    if resultList[j][-1].isdigit():
+                        score = score[::-1]
+                        score += resultList[j][-1]
+                        score = score[::-1]
+                        resultList[j] = resultList[j][:len(resultList[j])-1]
+                    else: 
+                        break
+                resultList.insert(j+1, score)
+                # print('--------------分割成绩结束--------------')
+            result[resultList[j]] = resultList[j+1]
+            j += 2
+    print('数据如下')
+    print(result)
     return result
 
 
-
-def writeData(result):
-    # print(result)
-    xlsx = load_workbook('score.xlsx')
-    FuBen = xlsx['副本']
-    JiaZuZhan = xlsx['家族战33']
-    ShenYuan = xlsx['深渊']
-
+def getNameList():
+    global excelPath
+    inputPath = input('请输入表格名 (默认为: 胧月成绩统计表.xlsx):')
+    if inputPath != '': excelPath = inputPath
+    excelPath = os.path.dirname(os.path.realpath(sys.executable)) + '/' + excelPath
+    excel = load_workbook(excelPath)
+    FuBen = excel['副本']
     nameCells = FuBen['B']
-    nameList = []
     for cell in nameCells :
         if (cell.value != 'id') and (cell.value != None) and cell.fill.start_color.index != 1 :
             nameList.append(str(cell.value))
-    # print(names)
 
-    for name, score in result.items() :
-        # print('name:', name, ' score:', score)
-        pos = nameList.index(name) if (name in nameList) else -1
-        if pos >= 0 :
-            print('姓名：', name, '\t分数：', score, '\t行数', pos + 2)
-        else :
+
+def processData(result):
+    getNameList()
+    col = writeData(result)
+    decorateData(col)
+
+
+def writeData(OCRResult):
+    xlsx = load_workbook(excelPath)
+    FuBen = xlsx['副本']
+    # JiaZuZhan = xlsx['家族战33']
+    # ShenYuan = xlsx['深渊']
+
+    col = FuBen.max_column + 1
+    inputCol = input('将在表格第%s列插入数据。输入数字更改，或enter确认:' %(col))
+    if inputCol != '': col = int(inputCol)
+    for name, score in OCRResult.items() :
+        row = nameList.index(name) if (name in nameList) else -1
+        if row < 0 :
             maxSame = 0
             for i in range(len(nameList) - 1) :
                 currentSame = fuzz.partial_ratio(nameList[i], name)
                 if currentSame > maxSame :
                     maxSame = currentSame
-                    pos = i
-            print('姓名：', name, '\t最相似姓名：', nameList[pos], '\t分数：', score, '\t行数：', i + 2, '\t匹配度：', maxSame)
+                    row = i
+            if row < 0 :
+                print("\033[1;37;41mERROR\033[0m 没有找到族员:\033[0;30;47m%s\033[0m。他的成绩是:\033[0;37;44m%s\033[0m。请确认他是否改名。不然就是程序出错了诶嘿😛" % (name,score))
+                continue
+            else :
+                print("\033[1;37;43mWARN\033[0m 没有找到族员:\033[0;30;47m%s\033[0m，名字最接近的族员是:\033[0;30;47m%s\033[0m。他的成绩是:\033[0;37;44m%s\033[0m。请留意匹配是否出错。" % (name,nameList[row],score))
+        row += 3
+        FuBen.cell(row,col).value = int(score)
+    
+    xlsx.save(excelPath)
+    print("\033[1;37;42mSUCCESS\033[0m 数据录入成功！")
+    return col
+
+
+def decorateData(col):
+    xlsx = load_workbook(excelPath)
+    FuBen = xlsx['副本']
+    classify = classifyJson[FBType[type]]
+    colorList = classifyJson['color']
+    FuBen.cell(2, col).value = FBType[type]
+
+    border = Border(left=Side(border_style='thin', color='000000'), right=Side(border_style='thin', color='000000'), top=Side(border_style='thin',color='000000'), bottom=Side(border_style='thin',color='000000'))
+    for i in range(3, len(nameList)+ 3): 
+        color = ""
+        if FuBen.cell(i,col).value == None:
+            noScore = input("%s没有成绩，是否请假？(默认否,直接回车)[N/y]:" %(FuBen.cell(i,2).value))
+            if noScore == '': noScore = "N"
+            while noScore != 'N' and noScore != 'n' and noScore != 'Y' and noScore != 'y':
+                noScore = input("输入无效，请输入‘y’、‘n’或直接enter:")
+                if noScore == '': noScore = "N"
+            color = colorList['absence'] if noScore == "N" or noScore == "n" else colorList['leave']
+        elif int(FuBen.cell(i,col).value) >= classify['A']:
+            color = colorList['A']
+        elif int(FuBen.cell(i,col).value) < classify['A'] and int(FuBen.cell(i,col).value) >= classify['B']:
+            color = colorList['B']
+        elif  int(FuBen.cell(i,col).value) < classify['B'] and int(FuBen.cell(i,col).value) >= classify['C']:
+            color = colorList['C']
+        else:
+            color = colorList['D']
+        FuBen.cell(i, col).fill = PatternFill(fill_type = 'solid', start_color=color, end_color=color)
+        FuBen.cell(i, col).border = border
+    
+    xlsx.save(excelPath)
+    print("\033[1;37;42mSUCCESS\033[0m 颜色填充成功！")
+
 
 if __name__ == '__main__' :
     result = getImgInfo()
-    writeData(result)
+    processData(result)
+    input("登记完毕。按任意键结束程序……")
 
 
 
